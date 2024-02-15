d88e468982ffa32c0790199ada30c2261e8ca211
Mistoruu <150136429+Mistoruu@users.noreply.github.com>
2024-02-15 12:15:35 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
     class GameCharacter extends Character
     {
 
-        // atributs
+        // attributs
         public $Name;
         public $Picture;
         public $Description;
@@ -55,6 +55,40 @@
         }
     }
 
+    class MangaCharacter extends Character
+    {
+        // attributs
+        public $Name;
+        public $Picture;
+        public $Description;
+
+        // Methods / Functions
+        public function __construct($Name, $Picture, $Description)
+        {
+            $this->Name = $Name;
+            $this->Picture = $Picture;
+            $this->Description = $Description;
+
+            echo "<h2>" . $this->Name . "<h2>";
+        }
+        public function __destruct()
+        {
+        }
+
+        public function generateHTML()
+        {
+            echo "<h2>" . $this->Name . "<h2>";
+            echo "<div>";
+            echo "<img src'" . $this->Picture . "' alt='" . $this->Name . "' />";
+            echo "<p>" . $this->Description . "</p>";
+            echo "</div>";
+        }
+    }
+
+
+
+
+
     ?>
 
     <?php
@@ -62,12 +96,20 @@
     $Malenia = new GameCharacter("Malenia", "malenia.jpg", "I am Malenia Blade of Miquella and i have never known defeat");
     $Malenia->generateHTML();
 
+    $Joseph = new MangaCharacter("Joseph", "Joseph.jpg", "Nigurundayo");
+    $Joseph->generateHTML();
 
 
 
+    //cookies
+    $CookieName = "Ramirez";
+    $CookieValue = "Fortnite Game Character";
+    setCookie($CookieName, $CookieValue, time() + (86400 * 30));
 
 
-
+    if (!isset($_COOKIE[$CookieName])) {
+        echo "Cookie Value" . $_COOKIE[$CookieValue];
+    }
 
 
     ?>
